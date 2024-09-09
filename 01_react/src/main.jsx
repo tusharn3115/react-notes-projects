2a2db35c69223d6336f81e0e829b92bdf1afe1cb
Tushar Negi <negitushar81@gmail.com>
2024-09-09 16:26:06 +0530

    .

--- 01_react/src/main.jsx
+++ 01_react/src/main.jsx
@@ -5,6 +5,9 @@ import App from './App.jsx'
 // creating element using react elements
 // in custom react folder we have made a custom react where we have given props regarding to our liking but in react we cannot give methods and its properties to our liking, here react give an object with pre-defined syntax and we cannot control it  
 
+
+// so here we see how to create a react element in which first parameter is expected a html tag, second param is object and third is direct text
+
 const reactElement = React.createElement(
     'a',
     {href: "http://google.com", target: "_blank"},
