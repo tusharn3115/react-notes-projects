5af5e8657ef39c753d530865cac92ee6d3d8944a
Tushar Negi <negitushar81@gmail.com>
2024-09-12 18:13:59 +0530

    .

--- 03_props/src/App.jsx
+++ 03_props/src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/card'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Tailwind Test</h1>
+    <h1 className='bg-slate-800 text-white p-5 mb-9 rounded text- font-medium'>Props In React</h1>
+    <Card/>
+    <Card/>
     </>
   )
 }
