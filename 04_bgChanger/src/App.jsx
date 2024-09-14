b84d40c39994214f88e76f37b43d7cc3f0804455
Tushar Negi <negitushar81@gmail.com>
2024-09-14 16:35:58 +0530

    .

---
+++ 04_bgChanger/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react"
+
+function App() {
+  const [color, setColor] = useState("olive")
+
+  return (
+    <div className="w-full h-screen duration-200" style={{ backgroundColor: color }}>
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-xl bg-white px-3 py-2 rounded-3xl">
+          <button
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "red" }}>
+            Red
+          </button>
+          <button
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "green" }}>
+            Green
+          </button>
+          <button
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "blue" }}>
+            Blue
+          </button>
+          <button
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "yellow" }}>
+            Yellow
+          </button>
+          <button
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "pink" }}>
+            Pink
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
