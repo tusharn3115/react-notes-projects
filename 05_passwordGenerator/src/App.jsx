bb4c88706e8cb5a4153100a7de9ce61c7bcc5093
Tushar Negi <negitushar81@gmail.com>
2024-09-16 19:09:24 +0530

    .

--- 05_passwordGenerator/src/App.jsx
+++ 05_passwordGenerator/src/App.jsx
@@ -25,16 +25,16 @@ function App() {
   }, [length, numAllow, charAllow, setPassword])
 
   return (
-    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
+    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800">
       <h1 className='text-white text-center my-3'>Password generator</h1>
-    <div className="flex shadow rounded-lg overflow-hidden mb-4">
-        <input
-            type="text"
-            value={password}
-            className="outline-none w-full py-1 px-3"
-            placeholder="Password"
-            readOnly
-        />
+      <div className="flex shadow rounded-lg overflow-hidden mb-4">
+         <input
+             type="text"
+             value={password}
+             className="outline-none w-full py-1 px-3"
+             placeholder="Password"
+             readOnly
+         />
       </div>
     </div>
   )
