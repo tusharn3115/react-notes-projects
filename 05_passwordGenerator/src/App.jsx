65adeb9eaf970296a592f994c6b0c01a5491398d
Tushar Negi <negitushar81@gmail.com>
2024-09-16 18:23:23 +0530

    .

--- 05_passwordGenerator/src/App.jsx
+++ 05_passwordGenerator/src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length, setLength] = useState(8)
 
   return (
     <>
-    <h1 className='text-4xl text-center'>Password Generator</h1>
+    <h1 className='text-4xl text-center text-white'>Password Generator</h1>
     </>
   )
 }
