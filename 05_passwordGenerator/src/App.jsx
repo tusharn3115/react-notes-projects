016e411efb537781dde05eec00de80ddff7ec0e5
Tushar Negi <negitushar81@gmail.com>
2024-09-16 19:05:58 +0530

    .

--- 05_passwordGenerator/src/App.jsx
+++ 05_passwordGenerator/src/App.jsx
@@ -1,5 +1,4 @@
 import { useCallback, useState } from 'react'
-import './App.css'
 
 function App() {
   const [length, setLength] = useState(8)
@@ -12,12 +11,32 @@ function App() {
   const passwordGenerator = useCallback(() => {
     let pass = ""
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+    if(numAllow) str += "0123456789"
+    if(charAllow) str += "!@#$%&*+-"
+
+    for (let i = 1; i <= array.length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1)
+      pass = str.charAt(char)
+    }
+
+    setPassword(pass)
+
   }, [length, numAllow, charAllow, setPassword])
 
   return (
-    <>
-    <h1 className='text-4xl text-center text-white'>Password Generator</h1>
-    </>
+    <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-grey-800'>
+      <h1 className='text-white text-center'>Password Generator</h1>
+      <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+        <input 
+        type="text" 
+        value={password}
+        className='outline-none w-full py-1 px-3'
+        placeholder='Password...'
+        readOnly
+        />
+      </div>
+    </div>
   )
 }
 
