873d77b34dc49c4b350292698f7eea7c0e4aff73
Tushar Negi <negitushar81@gmail.com>
2024-09-17 18:18:37 +0530

    .

--- 05_passwordGenerator/src/App.jsx
+++ 05_passwordGenerator/src/App.jsx
@@ -12,8 +12,8 @@ function App() {
     let pass = ""
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
-    if(numAllow) str += "0123456789"
-    if(charAllow) str += "!@#$%&*+-"
+    if (numAllow) str += "0123456789"
+    if (charAllow) str += "!@#$%&*+-"
 
     for (let i = 1; i <= array.length; i++) {
       let char = Math.floor(Math.random() * str.length + 1)
@@ -28,13 +28,14 @@ function App() {
     <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800">
       <h1 className='text-white text-center my-3'>Password generator</h1>
       <div className="flex shadow rounded-lg overflow-hidden mb-4">
-         <input
-             type="text"
-             value={password}
-             className="outline-none w-full py-1 px-3"
-             placeholder="Password"
-             readOnly
-         />
+        <input
+          type="text"
+          value={password}
+          className="outline-none w-full py-1 px-3"
+          placeholder="Password"
+          readOnly
+        />
+        <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>copy</button>
       </div>
     </div>
   )
