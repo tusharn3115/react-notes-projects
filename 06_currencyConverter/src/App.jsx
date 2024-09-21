13b85e088589778b6d3056a5d7a8af753f440536
Tushar Negi <negitushar81@gmail.com>
2024-09-21 12:26:30 +0530

    .

--- 06_currencyConverter/src/App.jsx
+++ 06_currencyConverter/src/App.jsx
@@ -1,16 +1,17 @@
 import { useState } from 'react'
-import InputBox from './components'
+import {InputBox} from './components'
 import useCurrencyInfo from './hooks/useCurrencyInfo'
-import './App.css'
+
 
 function App() {
+
   const [amount, setAmount] = useState(0)
   const [from, setFrom] = useState("usd")
   const [to, setTo] = useState("inr")
   const [convertedAmount, setConvertedAmount] = useState(0)
 
+  const currencyInfo = useCurrencyInfo(from)
 
-  const currencyInfo = useCurrencyInfo(form)
   const options = Object.keys(currencyInfo)
 
   const swap = () => {
@@ -19,16 +20,16 @@ function App() {
     setConvertedAmount(amount)
     setAmount(convertedAmount)
   }
-
+  
   const convert = () => {
     setConvertedAmount(amount * currencyInfo[to])
   }
 
-    return (
+  return (
     <div
         className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
         style={{
-            backgroundImage: `url('https://images.pexels.com/photos/5466785/pexels-photo-5466785.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')`,
+            backgroundImage: `url('https://images.pexels.com/photos/3532540/pexels-photo-3532540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')`,
         }}
     >
         <div className="w-full">
